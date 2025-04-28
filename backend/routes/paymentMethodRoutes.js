dda1799d76ca7e39e2ae11caeb7e6c8d56639555
Taha-3 <90838393+Taha-3@users.noreply.github.com>
2025-04-28 23:13:37 +0500

    Delete backend/routes/paymentMethodRoutes.js

--- backend/routes/paymentMethodRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {addPaymentMethod} = require('../Controllers/paymentMethodController')
-
-const router = express.Router();
-
-router.post('/addPaymentMethod', addPaymentMethod);
-
-module.exports = router;
