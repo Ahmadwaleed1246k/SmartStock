b9b1f93fab67dfe957d158b5aa24dc1e12fb3676
Taha-3 <90838393+Taha-3@users.noreply.github.com>
2025-04-28 23:04:53 +0500

    Update purchaseRoutes.js

--- backend/routes/purchaseRoutes.js
+++ backend/routes/purchaseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {createPurchase} = require('../Controllers/purchaseController')
-
+const { createPurchase, getAllPurchases, getNextVoucherNo } = require('../Controllers/purchaseController');
 const router = express.Router();
 
-router.post('/addPurchase', createPurchase);
+router.post('/add-purchase', createPurchase);
+router.get('/getAllPurchases', getAllPurchases);
+router.post('/get-next-voucher', getNextVoucherNo);
 
 module.exports = router;
