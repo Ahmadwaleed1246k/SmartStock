e1e3c692322968b2bd7e913b850419569f8b5ac8
Taha-3 <90838393+Taha-3@users.noreply.github.com>
2025-05-06 01:58:48 +0500

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const{createPayment, getNextVoucherNo} = require('../Controllers/paymentController');
+const{createPayment, getNextVoucherNo, getPaymentDueNotifications} = require('../Controllers/paymentController');
 
 // Create a new payment
 router.post('/create', createPayment);
 
 // Get next voucher number
 router.post('/get-next-voucher', getNextVoucherNo);
+router.post('/get-payment-due-notifications', getPaymentDueNotifications);
 
 module.exports = router;
