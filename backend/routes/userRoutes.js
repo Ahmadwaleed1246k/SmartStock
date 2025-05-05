afc97ef0109377a8d65cab4830289585f5332126
Taha-3 <90838393+Taha-3@users.noreply.github.com>
2025-05-06 02:00:31 +0500

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,18 @@
 const express = require('express');
-const {
-  addUser,
-  addEmployee, // Add this import
-  getUsersByCompID, 
-  loginUser, 
-  getSessionData, 
-  getUsersByUserID, 
-  changePassword, 
-  deleteUser,
-  getEmployeesByCompany,
-  editEmployee // Add this
-} = require('../Controllers/userController');
+const {addUser, getUsersByCompID, loginUser, getSessionData, getUsersByUserID, 
+    addEmployee, editEmployee, changePassword, deleteUser,
+     getEmployeesByCompany} = require('../Controllers/userController');
 
 const router = express.Router();
 
 router.post('/add-User', addUser);
-router.post('/add-employee', addEmployee); // Add this new route
 router.post('/get-user-by-compID', getUsersByCompID);
 router.post('/login', loginUser);
 router.get('/session', getSessionData);
 router.post('/get-users-by-user-id', getUsersByUserID);
 router.post('/change-password', changePassword);
 router.post('/delete-user', deleteUser);
-router.post('/edit-employee', editEmployee); // Add this new route
 router.post('/get-employees-by-company', getEmployeesByCompany);
-
+router.post('/add-employee', addEmployee);
+router.post('/edit-employee', editEmployee);
 module.exports = router;
