4a924a9107ebbf768177d3bdff448cc2dc0edfa9
Taha-3 <90838393+Taha-3@users.noreply.github.com>
2025-05-06 01:59:08 +0500

    Update productCatRoutes.js

--- backend/routes/productCatRoutes.js
+++ backend/routes/productCatRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const {addProductCategory} = require('../Controllers/productCatController')
+const {addProductCategory, getProductCategoriesByCompID, deleteProductCategory, getProductCategoriesByGroupID} = require('../Controllers/productCatController')
 
 const router = express.Router();
 
 router.post('/addProductCategory', addProductCategory);
+router.post('/getProductCategoriesByCompID', getProductCategoriesByCompID)
+router.post('/delete-category', deleteProductCategory);
+router.post('/getProductCategoriesByGroupID', getProductCategoriesByGroupID);
 
 module.exports = router;
